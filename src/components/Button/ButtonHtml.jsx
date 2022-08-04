89df940bf20ab5fc30f88c169831a309cfbd642a
Anusuya Balakrishnan <anusuyabalakrishnan01@gmail.com>
2022-08-04 19:48:47 +0530

    course card responsive 4.8.2022

---
+++ src/components/Button/ButtonHtml.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <div id="Button">
+      <h1>For More</h1>
+    </div>
+  );
+}
--- src/components/Course/CourseHtml.jsx
+++ src/components/Course/CourseHtml.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CourseStyle from "./CourseStyle.css";
 import image from "./image/image.jpeg";
-
+import { IoTime } from "react-icons/io5";
+import { MdEventNote } from "react-icons/md";
+import Button from "../Button/ButtonHtml";
 export default function Course() {
   return (
     <section id="CourseHtml">
       <div className="CourseParent">
-        <div className="CourseImage">
-          <img src={image} alt="courseImage" />
-        </div>
+        <img src={image} alt="courseImage" />
         <div className="CourseDetails">
           <p className="CourseTitle">Preliminary Exam Course</p>
           <p className="CourseContent">
@@ -16,8 +16,18 @@ export default function Course() {
             rhoncus in eget lectus quis consectetur facilisis. Egestas posuere
             nunc donec ultricies egestas vitae id in.
           </p>
-          <div className="courseDuration"></div>
+          <div className="courseDuration">
+            <div className="duration">
+              <IoTime />
+              <p>8 Months</p>
+            </div>
+            <div className="duration">
+              <MdEventNote />
+              <p>80 Tests</p>
+            </div>
+          </div>
         </div>
+        <Button />
       </div>
     </section>
   );
